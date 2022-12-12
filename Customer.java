4bc04bcb5059f72a481ab4844f4aae0fde2fc992
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:36:41 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,29 +2,36 @@ package com.database.onlinefood.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
-@AllArgsConstructor
+@ToString
 @NoArgsConstructor
-@Table
+@AllArgsConstructor
+@Table(name = "customer")
 public class Customer {
+
 	@Id
-	@Column(name = "id")
-	private int id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "customerid")
+	private Integer customerid;
 	@Column(name = "username")
 	private String username;
 	@Column(name = "password")
 	private String password;
-	@Column(name = "email")
-	private String email;
-
 	
+	public Customer(String username, String password)
+	{
+		this.username=username;
+		this.password=password;
+	}
 }
