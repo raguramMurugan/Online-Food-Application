b1b981faf6435c5d5a1665765b1311884c6dd23c
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:36:12 +0530

    Update AddToCart.java

--- AddToCart.java
+++ AddToCart.java
@@ -2,30 +2,34 @@ package com.database.onlinefood.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Table
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
+@Table(name = "add_to_cart")
 public class AddToCart {
 
 	@Id
-	@Column(name = "id")
-	private int id;
-	@Column(name = "product_id")
-	private int productId;
-	@Column(name = "product_name")
-	private String productName;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "cartid")
+	private Integer cartid;
+	@Column(name = "menuid")
+	private Integer menuid;
+	@Column(name = "foodname")
+	private String foodname;
 	@Column(name = "quantity")
-	private int quantity;
-	@Column(name = "price")
-	private double price;
-
+	private Integer quantity;
+	@Column(name = "amount")
+	private double amount;
 }
