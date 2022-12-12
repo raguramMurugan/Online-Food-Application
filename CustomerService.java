79df3d0faa3e7a9915b0efd69cd642396b4d67ae
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:37:20 +0530

    Update CustomerService.java

--- CustomerService.java
+++ CustomerService.java
@@ -1,8 +1,27 @@
 package com.database.onlinefood.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.database.onlinefood.entity.Customer;
+import com.database.onlinefood.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerrepo;
+
+	public Integer isCustomerPresentInDatabase(Customer customer) {
+		Customer customer1=customerrepo.findCustomerByUsernameAndPassword(customer.getUsername(), customer.getPassword());
+		
+		return customer1!=null? customer1.getCustomerid() :null;
+	}
+
+	public Customer saveCustomer(Customer customer) {
+		
+		return customerrepo.save(customer);
+		
+	}
 
 }
