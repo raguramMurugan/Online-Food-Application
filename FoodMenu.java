8a5beff89b3905177b06cbe72a9b7c9493d506b5
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:37:47 +0530

    Update FoodMenu.java

--- FoodMenu.java
+++ FoodMenu.java
@@ -2,30 +2,33 @@ package com.database.onlinefood.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table
-public class FoodMenu {
+@ToString
+@Table(name = "menu")
+public class Menu {
+
 	@Id
-	@Column(name = "id")
-	private int id;
-	@Column(name = "food_name")
-	private String foodName;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "menuid")
+	private Integer menuid;
+	@Column(name = "foodname")
+	private String foodname;
 	@Column(name = "available_quantity")
-	private int availableQuantity;
+	private Integer availableQuantity;
 	@Column(name = "price")
 	private double price;
-
-
 	
 }
