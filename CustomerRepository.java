bdfcb4174758aecd5a7aed349e5aef78c90f58c4
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:41:45 +0530

    Update CustomerRepository.java

--- CustomerRepository.java
+++ CustomerRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.database.onlinefood.entity.Customer;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+
+	Customer findCustomerByUsernameAndPassword(String username, String password);
 
 }
