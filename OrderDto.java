2e043dbd60aec1ba37b3c6de0b5922aa18dabdf4
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-12 19:39:39 +0530

    Update OrderDto.java

--- OrderDto.java
+++ OrderDto.java
@@ -7,15 +7,19 @@ import com.database.onlinefood.entity.AddToCart;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrderDto {
 	
-	private String description;
+	private String suggestion;
 	private List<AddToCart> cart;
-	private String email;
-	private String username;
 	
+	private String username;
+	private String password;
+	
+
 }
