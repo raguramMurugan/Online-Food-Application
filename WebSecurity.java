058c28462a0259361d2efc827d0bf0d0739386a8
Raguram Murugan <116337952+raguramMurugan@users.noreply.github.com>
2022-12-13 19:24:44 +0530

    Updated the Configuration Class

--- WebSecurity.java
+++ WebSecurity.java
@@ -3,5 +3,49 @@ package com.database.onlinefood.config;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class WebSecurity extends WebSecurityConfigurerAdapter{
+ 
+private static final String Unblock_urls= 
+			 "/Kollapasi/loginPage";
+	
+	@Autowired
+	UserDetailsService userDetailsService;
 
+	AuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		return provider;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.cors()
+		.and()
+		.csrf()
+		.disable()
+		.authorizeHttpRequests()
+		.antMatchers(Unblock_urls).permitAll()
+		.and()
+		.httpBasic();
+	}
+
+
+	@Bean
+	PasswordEncoder passwordEncoder()
+	{
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Bean(name=BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		
+		return super.authenticationManagerBean();
+	}
 }
